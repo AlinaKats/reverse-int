ece5f617c1c9b0db79c57316fd00f19766f297c1
AlinaKats <alina.emelianova91@gmail.com>
2022-08-04 12:11:15 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let numToStr = n.toString();
+    let numToStrPositive = numToStr.replace(/[\-]/, '');
+    console.log(numToStrPositive);
+
+    let arr = numToStrPositive.split('').reverse();
+    let str = arr.join('');
+
+    console.log(str);
+
+    return +(str);
 }
